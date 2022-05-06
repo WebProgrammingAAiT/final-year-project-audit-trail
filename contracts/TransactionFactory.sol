0becedc93f62bb33bbbfe3208ce3b5e452bb0f91
Beemnet <bemnetteklu@gmail.com>
2022-05-06 20:44:30 +0300

    optimized functions and event interaction

--- contracts/TransactionFactory.sol
+++ contracts/TransactionFactory.sol
@@ -9,13 +9,13 @@ contract TransactionFactory {
 //   using SafeMath16 for uint16;
 
 
-    event NewTransaction(string id, string receiptNumber, User user, string department, string source, uint requiredDate, uint returnedDate);
     event NewReturningTransaction(string id, string receiptNumber, string user, string department, uint returnedDate, string[] returnedItems);
     event NewReceivingTransaction(string id, string receiptNumber, string user, string source, string[] receivedItems);
     event NewRequestingTransaction(string id, string receiptNumber, string user, string department, uint requiredDate, string[] requestedItems);
     event NewTransferredTransaction(string id, string receiptNumber, string department, string requestingTransaction, string transferredItem );
 
     event NewUser(string id, string username, string email,  string role);
+    event NewDepartment(string id, string name);
     event UpdateRole(string username, string oldRole, string newRole);
     event UpdatePassword(string username, string message);
     event NewItem(string id, Department department, ItemType itemType, uint32 price);
@@ -77,7 +77,6 @@ contract TransactionFactory {
         mapping(uint => string) returnedItems;
         mapping(uint => string) requestedItems;
         mapping(uint => string) receivedItems;
-
     }
 
     Department[] public departments;
@@ -85,11 +84,11 @@ contract TransactionFactory {
     Item[] public items;
     User[] public users;
     Subinventory[] public subinventories;
-    //ItemsOfInterest[] public itemsOfInterest;
 
+    mapping(string => ItemsOfInterest) list;
     mapping(uint => Transaction) transactions;
     uint32 transactionSize = 0;
-    Transaction[] public transactionsArray;
+    Transaction[] public transactionsArray; // when to use
     
     string[] public dataHashes; 
     mapping (bytes32 => Transaction) hashToTransaction;
@@ -118,11 +117,11 @@ contract TransactionFactory {
         emit UpdatePassword(username, message);
     }
 
-    function _createDepartment(string memory id, string memory name)  external {
+    function createDepartment(string memory id, string memory name)  external {
         departments.push(Department(id,name));
     }
 
-    function _createItemType(string memory id, string memory name, string memory itemCode) external {
+    function createItemType(string memory id, string memory name, string memory itemCode) external {
         itemTypes.push(ItemType(id, name, itemCode));
     }
 
@@ -140,77 +139,56 @@ contract TransactionFactory {
         return ItemsOfInterest(status, itemType, item, quantity, unitCost); //to be called from interact.js prior to calling createTransaction
     }
 
-    function createTransaction(string memory id, string memory receiptNumber, string memory user) public{ 
+    function returnedTransaction(string memory id, string memory receiptNumber, string memory user, string memory department, uint returnedDate, string[] memory itemsOfInterest) public {
         uint transactionsID = transactionSize++;
         Transaction storage t = transactions[transactionsID];
         t.id = id;
         t.receiptNumber = receiptNumber;
         t.user = user;
+        t.department = department;
+        t.returnedDate = returnedDate;
+        for(uint j=0; j<itemsOfInterest.length; j++){
+            t.returnedItems[j] = itemsOfInterest[j];
+        }
+        emit NewReturningTransaction(t.id, t.receiptNumber, t.user, t.department, t.returnedDate, itemsOfInterest);
+    }
+    function receivedTransaction(string memory id, string memory receiptNumber, string memory user, string memory source,  string[] memory itemsOfInterest) public {
+        uint transactionsID = transactionSize++;
+        Transaction storage t = transactions[transactionsID];
+        t.id = id;
+        t.receiptNumber = receiptNumber;
+        t.user = user;
+        t.source = source;
+        for(uint j=0; j<itemsOfInterest.length; j++){
+            t.receivedItems[j] = itemsOfInterest[j];
+        }
+        emit NewReceivingTransaction(t.id, t.receiptNumber, t.user, t.source, itemsOfInterest);
     }
 
-    function returnedTransaction(string memory id, string memory department, uint returnedDate, string[] memory itemsOfInterest) public returns (bool) {
-        
-        for (uint i=0; i<=transactionSize; i++){
-            if (keccak256(abi.encodePacked(transactions[i].id)) == keccak256(abi.encodePacked(id))) {
-                Transaction storage t = transactions[i];
-                t.department = department;
-                t.returnedDate = returnedDate;
-                for(uint j=0; j<itemsOfInterest.length; j++){
-                    t.returnedItems[j] = itemsOfInterest[j];
-                }
-            //    transactionsArray.push(t);
-                emit NewReturningTransaction(t.id, t.receiptNumber, t.user, t.department, t.returnedDate, itemsOfInterest);
-                return true;
-            }
+    function requestingTransaction(string memory id, string memory receiptNumber, string memory user, string memory department, uint requiredDate, string[] memory itemsOfInterest) public {
+        uint transactionsID = transactionSize++;
+        Transaction storage t = transactions[transactionsID];
+        t.id = id;
+        t.receiptNumber = receiptNumber;
+        t.user = user;
+        t.department = department;
+        t.requiredDate = requiredDate;
+        for(uint j=0; j<itemsOfInterest.length; j++){
+            t.requestedItems[j] = itemsOfInterest[j];
         }
-        return false;
-    }
-    function receivedTransaction(string memory id, string memory source,  string[] memory itemsOfInterest) public returns (bool) {
-        
-        for (uint i=0; i<=transactionSize; i++){
-            if (keccak256(abi.encodePacked(transactions[i].id)) == keccak256(abi.encodePacked(id))) {
-                Transaction storage t = transactions[i];
-                t.source = source;
-                for(uint j=0; j<itemsOfInterest.length; j++){
-                    t.receivedItems[j] = itemsOfInterest[j];
-                }
-                emit NewReceivingTransaction(t.id, t.receiptNumber, t.user, t.source,  itemsOfInterest);
-                return true;
-            }
-        }
-        return false;
+        emit NewRequestingTransaction(t.id, t.receiptNumber, t.user, t.department, t.requiredDate, itemsOfInterest);        
     }
 
-    function requestingTransaction(string memory id, string memory department, uint requiredDate, string[] memory itemsOfInterest) public returns (bool) {
-        
-        for (uint i=0; i<=transactionSize; i++){
-            if (keccak256(abi.encodePacked(transactions[i].id)) == keccak256(abi.encodePacked(id))) {
-                Transaction storage t = transactions[i];
-                t.department = department;
-                t.requiredDate = requiredDate;
-                for(uint j=0; j<itemsOfInterest.length; j++){
-                    t.requestedItems[j] = itemsOfInterest[j];
-                }
-                emit NewRequestingTransaction(t.id, t.receiptNumber, t.user, t.department, t.requiredDate, itemsOfInterest);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function transferTransaction(string memory id, string memory department, string memory requestingTransactions, string memory itemsOfInterest) public returns (bool) {
-        
-        for (uint i=0; i<=transactionSize; i++){
-            if (keccak256(abi.encodePacked(transactions[i].id)) == keccak256(abi.encodePacked(id))) {
-                Transaction storage t = transactions[i];
-                t.department = department;
-                t.requestingTransaction = requestingTransactions;
-                t.transferredItem = itemsOfInterest;
-                emit NewTransferredTransaction(t.id, t.receiptNumber, t.department, t.requestingTransaction, t.transferredItem);
-                return true;
-            }
-        }
-        return false;
+    function transferTransaction(string memory id, string memory receiptNumber, string memory user, string memory department, string memory requestingTransactions, string memory itemsOfInterest) public {
+        uint transactionsID = transactionSize++;
+        Transaction storage t = transactions[transactionsID];
+        t.id = id;
+        t.receiptNumber = receiptNumber;
+        t.user = user;
+        t.department = department;
+        t.requestingTransaction = requestingTransactions;
+        t.transferredItem = itemsOfInterest;
+        emit NewTransferredTransaction(t.id, t.receiptNumber, t.department, t.requestingTransaction, t.transferredItem);
     }   
 
 
--- scripts/interact.js
+++ scripts/interact.js
@@ -4,105 +4,129 @@ const { API_URL, PRIVATE_KEY, API_KEY, CONTRACT_ADDRESS } = process.env
 
 const { ethers } = require("hardhat");
 const contract = require("../artifacts/contracts/AuditTrail.sol/AuditTrail.json");
-//console.log(JSON.stringify(contract.abi));
 
 const alchemyProvider = new ethers.providers.AlchemyProvider(network="rinkeby", API_KEY);
 const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
 const auditTrailContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
 
 async function main() {
+    // run demo functions here
 }
 async function createUser(id, name, username, role){
     const tx = await auditTrailContract.createUsers(id, name, username, role);
+    // TODO give specific args
+    // could help to show data on fe
+    auditTrailContract.on('NewUser', async (...args) =>  {
+        console.log("trans:", args[args.length - 1].transactionHash);
+    });
+    // TODO call audit for all trans loaded[iterate through an array of loaded transaction hash] 
     await tx.wait();
     console.log(tx.hash);
 }
 async function updateRole(username, role){
     const tx = await auditTrailContract.updateRole(username, role);
+    // TODO give specific args
+    auditTrailContract.on('UpdateRole', async (...args) =>  {
+        console.log("trans:", args[args.length - 1].transactionHash);
+    });
+    // TODO call audit for all trans loaded[iterate through an array of loaded transaction hash]
     await tx.wait();
     console.log(tx.hash);
 }
-async function printUsers(){
-    const tx = await auditTrailContract.users();
-    //for 
-    console.log(tx.toString());
-}
+
 async function createDepartment(id, name){
     const tx = await auditTrailContract._createDepartment(id, name);
+    // TODO give specific args
+    auditTrailContract.on('NewDepartment', async (...args) =>  {
+        console.log("trans:", args[args.length - 1].transactionHash);
+    });
+    // TODO call audit for all trans loaded[iterate through an array of loaded transaction hash]
     await tx.wait();
     console.log(tx.hash);
 }
 async function createItem(id, dept, itemType, price){
     const tx = await auditTrailContract.createUsers(id, dept, itemType, price);
+    // TODO give specific args
+    auditTrailContract.on('NewItem', async (...args) =>  {
+        console.log("trans:", args[args.length - 1].transactionHash);
+    });
+    // TODO call audit for all trans loaded[iterate through an array of loaded transaction hash] 
+    // maybe this was why he was doing the "can only be audited once" thing?
     await tx.wait();
     console.log(tx.hash);
 }
 async function createSubinvenory(id, name){
     const tx = await auditTrailContract.createSubinvenory(id, name);
+    // TODO give specific args
+    auditTrailContract.on('NewSubinventory', async (...args) =>  {
+        console.log("trans:", args[args.length - 1].transactionHash);
+    });
+    // TODO call audit for all trans loaded[iterate through an array of loaded transaction hash]
     await tx.wait();
     console.log(tx.hash);
 }
+
 async function createReturnTransaction() {
-    // const tx = await auditTrailContract.createTransaction("fifth", "005", "Neba");
-    // await tx.wait();
-    // console.log("hash: " + tx.hash);
-    
-    const items = ["obect1", "object2"];
-    const txs = await auditTrailContract.returnedTransaction("fifth", "ITSC", 12, items );
-    await txs.wait();
-    console.log("hash: " + txs.hash);
-    //console.log("data: " + txs.data);
-    
 
-}
-async function createTransactions(id, receiptNumber, user){
-    const tx = await auditTrailContract.createTransaction(id, receiptNumber, user);
-    await tx.wait();
-    console.log("hash: " + tx.hash);
-}
-
-async function createReceiveTransaction() {
-
-    // createTransactions("third", "004", "Bemnet Teklu");
     // TODO call createItemsOfInterest with predefined ids/addresses
     // TODO create an array of said ids and put it in items[]
-    // TODO said items passed to all specific transactionss.
-
-    const tx = await auditTrailContract.createTransaction("sixth", "002", "Neba");
+    // TODO said items passed to all specific transactions.
+    const items = ["obect1", "object2"];
+    // TODO give specific args
+    const tx = await auditTrailContract.returnedTransaction("fifth", "005", "Neba", "ITSC", 12, items );
+    auditTrailContract.on('NewReturningTransaction', async (...args) =>  {
+        console.log("trans:", args[args.length - 1].transactionHash);
+    });
+    // TODO call audit for all trans loaded[iterate through an array of loaded transaction hash]
     await tx.wait();
     console.log("hash: " + tx.hash);
+    //console.log("data: " + tx.data);
+}
+async function createReceiveTransaction() {
 
+    // TODO call createItemsOfInterest with predefined ids/addresses
+    // TODO create an array of said ids and put it in items[]
+    // TODO said items passed to all specific transactions.
     const items = ["obect1", "object2"];
-    const txs = await auditTrailContract.receivedTransaction("sixth", "Elec", items );
-    await txs.wait();
-    console.log("hash: " + txs.hash);
-    //console.log("data: " + txs.data);
-    // const message = await auditTrailContract.transactions();
-    // console.log("transactions ", message);
-    
-
+    // TODO give specific args
+    const tx = await auditTrailContract.receivedTransaction("sixth", "002", "Neba", "Elec", items );
+    auditTrailContract.on('NewReceivingTransaction', async (...args) =>  {
+        console.log("trans:", args[args.length - 1].transactionHash);
+    });
+    // TODO call audit for all trans loaded[iterate through an array of loaded transaction hash]
+    await tx.wait();
+    console.log("hash: " + tx.hash);
 }
 
 async function createRequestTransaction() {
 
-    // createTransactions("third", "004", "Bemnet Teklu");
     // TODO call createItemsOfInterest with predefined ids/addresses
     // TODO create an array of said ids and put it in items[]
-    // TODO said items passed to all specific transactionss.
-
-    const tx = await auditTrailContract.createTransaction("seventh", "007", "Neba");
-    await tx.wait();
-    console.log("hash: " + tx.hash);
-
+    // TODO said items passed to all specific transactions.
     const items = ["obect1", "object2"];
-    const txs = await auditTrailContract.requestingTransaction("seventh", "Elec", 112, items );
-    await txs.wait();
-    console.log("hash: " + txs.hash);
-    //console.log("data: " + txs.data);
-    console.log(txs);
-    // const message = await auditTrailContract.transactions();
-    // console.log("transactions ", message);
-    
-
+    // TODO give specific args
+    // it's printing hashes of past, mined transactions. ??? 
+    const tx = await auditTrailContract.requestingTransaction("second","002", "Neba", "Elec", 112, items );
+    auditTrailContract.on('NewRequestingTransaction', async (...args) =>  {
+        console.log("trans:", args[args.length - 1].transactionHash);
+    });
+    // TODO call audit for all trans loaded[iterate through an array of loaded transaction hash]
+    await tx.wait();
+    console.log("hash:", tx.hash);
 }
+async function createTransferTransaction() {
+    // TODO call createItemsOfInterest with predefined ids/addresses called item
+    // TODO said item passed to all specific transactions.
+    const item = ["object1"]; 
+    // TODO give specific args
+    const tx = await auditTrailContract.transferTransaction("second", "002","user", "department", "tranaction no_3", item);
+    auditTrailContract.on('NewTransferredTransaction', async (... args) => {
+        console.log("trans", args[args.length - 1].transactionHash);
+    });
+    // TODO make it quit checking
+    // TODO call audit for all trans loaded[iterate through an array of loaded transaction hash]
+    await tx.wait();
+    console.log("hash:", tx.hash);
+}
+
 main(); 
