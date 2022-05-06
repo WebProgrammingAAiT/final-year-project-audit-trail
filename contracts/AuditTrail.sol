7d55e699bd5d98f39949ca989964f2ed7f982beb
Beemnet <bemnetteklu@gmail.com>
2022-05-06 20:51:41 +0300

    version change for bug fixes involving abi.encodeCall and extended using_for directive

--- contracts/AuditTrail.sol
+++ contracts/AuditTrail.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >=0.7.3;
+pragma solidity >=0.8.13;
 pragma experimental ABIEncoderV2;
 
 import "./TransactionFactory.sol";
@@ -8,11 +8,11 @@ import "./TransactionFactory.sol";
 contract AuditTrail is TransactionFactory {
 
 
-    function loadTransactions(bytes28 transactionIdentifier, bytes32 dataHash) public view {
+    function auditTransaction(bytes28 transactionIdentifier, bytes32 dataHash) public view {
         // TODO load
     }
 
-    function validateTransactions(bytes28 transactionIdentifier, bytes32 dataHash) public returns (bool) {
+    function validateTransaction(bytes28 transactionIdentifier, bytes32 dataHash) public returns (bool) {
         //compare loaded hash to database 
     }
 
--- contracts/TransactionFactory.sol
+++ contracts/TransactionFactory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >=0.7.3;
+pragma solidity >=0.8.13;
 pragma experimental ABIEncoderV2;
 
 contract TransactionFactory {
