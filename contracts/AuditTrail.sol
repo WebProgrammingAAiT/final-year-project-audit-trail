3d7b1f7c5dc651bc31ab5bdb42725211a49f379d
Beemnet <bemnetteklu@gmail.com>
2022-05-12 19:03:54 +0300

    defined audit and validate functionalities

--- contracts/AuditTrail.sol
+++ contracts/AuditTrail.sol
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity >=0.8.13;
-pragma experimental ABIEncoderV2;
 
 import "./TransactionFactory.sol";
 
 contract AuditTrail is TransactionFactory {
 
+    event Audit(bytes28 transactionIdentifier, bytes32 dataHash);
+    mapping(bytes28 => bytes32) public dataHashes;
+    bytes28[] public auditedTransactions;
 
-    function auditTransaction(bytes28 transactionIdentifier, bytes32 dataHash) public view {
-        // TODO load
+    function auditTransaction(bytes28 transactionIdentifier, bytes32 dataHash) public {
+        require(dataHashes[transactionIdentifier] == 0, "A transaction can only be audited once");
+        dataHashes[transactionIdentifier] = dataHash;
+        auditedTransactions.push(transactionIdentifier);
+        emit Audit(transactionIdentifier, dataHash);
     }
 
-    function validateTransaction(bytes28 transactionIdentifier, bytes32 dataHash) public returns (bool) {
-        //compare loaded hash to database 
+    function validateTransaction(bytes28 transactionIdentifier, bytes32 dataHash) public view returns (bool) {
+        return dataHashes[transactionIdentifier] == dataHash ? true : false;
     }
-
-
 }
