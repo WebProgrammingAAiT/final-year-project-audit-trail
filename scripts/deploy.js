72cf4a27c9b83918ac7eaaa1a26e707c870fbe9e
Beemnet <bemnetteklu@gmail.com>
2022-05-22 10:54:56 +0300

    configuration files updated to run locally for easier development

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,20 @@
 async function main() {
-    const AuditTrail = await ethers.getContractFactory("AuditTrail");
+    // const AuditTrail = await ethers.getContractFactory("AuditTrail");
     console.log("Started deploying");
-    const audit_trail = await AuditTrail.deploy();
-    console.log("Contract deployed to address: ", audit_trail.address);
+    // const audit_trail = await AuditTrail.deploy();
+
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    // compiling our smart contract
+    const transactionContractFactory = await hre.ethers.getContractFactory("AuditTrail");
+    // deploying our smart contract after hardhat creates a local ethereum network
+    const transactionContract = await transactionContractFactory.deploy();
+    // probably waiting for the transaction to be mined
+    await transactionContract.deployed();
+    // getting the address of our deployed smart contract
+    console.log("Contract deployed to this address ", transactionContract.address);
+    console.log("Contract deployed by ", owner.address);
+
+    // console.log("Contract deployed to address: ", audit_trail.address);
 
 }
 
